d425c61b8b08b466e8a99a2fbee5ceceb6749ac7
Ali Ibrahim <165920255+Alishka-Space@users.noreply.github.com>
2024-09-24 14:56:15 +0200

    forms-login

--- src/pages/forms/Login.jsx
+++ src/pages/forms/Login.jsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './forms.css'
 
 const Login = () => {
-  return ( <div className="form-wrapper">
+  return (
+  <div className="form-wrapper">
     <form className="form">
-      <input type="email" placeholder='Email'/>
-      <input type="password" placeholder='password'/>
-      <button className="form-btn">Go</button>
+      <input type="email" placeholder='Email' />
+      <input type="password" placeholder='password' />
+      <button className="form-btn">Login</button>
     </form>
   </div>
-    
+
   )
 }
 
